5049c42832b2f9107851ab85b10a7a52654422c7
Uriel Benítez Medina <urielbenitez96@hotmail.com>
2021-09-19 19:20:58 -0500

    cambio de ruta

--- js/ErrorMessage.js
+++ js/ErrorMessage.js
@@ -10,7 +10,7 @@ const errorNotFound = () => {
   Swal.fire({
     title: "Upps 404!",
     text: "Lo sentimos GitHub user no encontrado.",
-    imageUrl: "./../assets/img/404.gif",
+    imageUrl: "../assets/img/404.gif",
     imageWidth: 200,
     imageHeight: 200,
     imageAlt: "404 picture",
